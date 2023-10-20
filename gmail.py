9ef9337c0855c5da2ed283c9791914f3ee418f51
Zeke Pulido <esp139@msstate.edu>
2023-10-19 22:36:31 -0500

    Added weather file and updated gmail to use the weather file for sending emails

--- gmail.py
+++ gmail.py
@@ -2,18 +2,21 @@ import os
 import smtplib
 import ssl
 from email.message import EmailMessage
+from weather import email
 
 # Define email sender and receiver
-email_sender = 'sender'
-email_password = 'password'
-email_receiver = 'reciever'
+email_sender = 'sender email'
+email_password = 'password'  # Replace with your actual email password
+email_receiver = 'reciever email'
+
+text = email()
 
 # Set the subject and body of the email
-subject = 'TEST!'
-body = """
+subject = 'Weather'
 
-Your email is here:
-
+# Use triple-quoted string to format the body
+body = f"""\
+{text}
 """
 
 em = EmailMessage()
@@ -28,4 +31,4 @@ context = ssl.create_default_context()
 # Log in and send the email
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
-    smtp.sendmail(email_sender, email_receiver, em.as_string())
+    smtp.send_message(em)
---
+++ weather.py
@@ -0,0 +1,48 @@
+import requests
+
+api_key = '46b2e1d6b5adce1b8a59d7397849bd40'
+
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
+
+CITY = 'Starkville,us'  # The city parameter should be in the format 'city,country'
+
+url = BASE_URL + "q=" + CITY + "&appid=" + api_key
+
+def kelvin_to_celsius_to_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = (celsius * 1.8) + 32
+    return fahrenheit
+
+def statement(max):
+    if max > 75:
+        return "It is going to be a hotter day!"
+    elif max < 65:
+        return "It is going to be a cooler day!"
+    else:
+        return "It will feel great today!"
+
+def email():
+    response = requests.get(url)
+    data = response.json()
+
+    curr_temp_kelvin = data['main']['temp']
+    curr_temp_fahrenheit = round(kelvin_to_celsius_to_fahrenheit(curr_temp_kelvin))
+
+    max_temp_kelvin = data['main']['temp_max']
+    max_temp_fahrenheit = round(kelvin_to_celsius_to_fahrenheit(max_temp_kelvin))
+
+    min_temp_kelvin = data['main']['temp_min']
+    min_temp_fahrenheit = round(kelvin_to_celsius_to_fahrenheit(min_temp_kelvin))
+
+    clouds = data['weather'][0]['description']
+
+    final = statement(max_temp_fahrenheit)
+
+    return f'''
+    Good morning,\n
+    The current temperature is: {curr_temp_fahrenheit} degrees outside.
+    The high of today is: {max_temp_fahrenheit} degrees outside.
+    The low of today is: {min_temp_fahrenheit} degrees outside.
+    The clouds are currently {clouds}\n
+    {final}
+    '''
